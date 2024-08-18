1a2b534052b2e7510efcf49dd1c003883ab33254
furkaniscn <furkan.iscn@outlook.com>
2024-08-19 01:09:34 +0300

    BookingProcessHasBeenDone

---
+++ ApiConsume/HotelProject.WebApi/Controllers/BookingController.cs
@@ -0,0 +1,55 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+
+        private readonly IBookingService _bookingservice;
+        public BookingController(IBookingService bookingservice)
+        {
+            _bookingservice = bookingservice;
+        }
+
+        [HttpGet]
+        public IActionResult BookingList()
+        {
+            var values = _bookingservice.TGetList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult AddBooking(Booking booking)
+        {
+            _bookingservice.TInsert(booking);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBooking(int id)
+        {
+            var values = _bookingservice.TGetById(id);
+            _bookingservice.TDelete(values);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBooking(Booking booking)
+        {
+            _bookingservice.TUpdate(booking);
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBooking(int id)
+        {
+            var values = _bookingservice.TGetById(id);
+            return Ok(values);
+        }
+    }
+}
