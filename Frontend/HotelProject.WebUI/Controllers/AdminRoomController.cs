72e0d81dc5017473a1dfd17daef0cc79a73cb7f0
furkaniscn <furkan.iscn@outlook.com>
2024-09-04 01:43:38 +0300

    RoomClassHasBeenUpdated

--- Frontend/HotelProject.WebUI/Controllers/AdminRoomController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminRoomController.cs
@@ -1,16 +1,15 @@
-﻿using HotelProject.WebUI.Models.Staff;
-using HotelProject.WebUI.Models.Testimonial;
+﻿using HotelProject.WebUI.Dtos.RoomDto;
+using HotelProject.WebUI.Models.Staff;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
-    public class TestimonialController : Controller
+    public class AdminRoomController : Controller
     {
-
         private readonly IHttpClientFactory _httpClientFactory;
-        public TestimonialController(IHttpClientFactory httpClientFactory)
+        public AdminRoomController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
@@ -18,38 +17,37 @@ namespace HotelProject.WebUI.Controllers
         public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("http://localhost:5266/api/Testimonial/");
+            var responseMessage = await client.GetAsync("http://localhost:5266/api/Room");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<TestimonialViewModel>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultRoomDto>>(jsonData);
                 return View(values);
             }
             return View();
         }
         [HttpGet]
-        public IActionResult AddTestimonial()
+        public IActionResult AddRoom()
         {
             return View();
         }
-
         [HttpPost]
-        public async Task<IActionResult> AddTestimonial(TestimonialViewModel model)
+        public async Task<IActionResult> AddRoom(CreateRoomDto createRoomDto)
         {
             var client = _httpClientFactory.CreateClient();
-            var jsonData = JsonConvert.SerializeObject(model);
+            var jsonData = JsonConvert.SerializeObject(createRoomDto);
             StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            var responseMessage = await client.PostAsync("http://localhost:5266/api/Testimonial/", stringContent);
+            var responseMessage = await client.PostAsync("http://localhost:5266/api/Room/", stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
             }
             return View();
         }
-        public async Task<IActionResult> DeleteTestimonial(int id)
+        public async Task<IActionResult> DeleteRoom(int id)
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.DeleteAsync($"http://localhost:5266/api/Testimonial/{id}");
+            var responseMessage = await client.DeleteAsync($"http://localhost:5266/api/Room/{id}");
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
@@ -57,25 +55,25 @@ namespace HotelProject.WebUI.Controllers
             return View();
         }
         [HttpGet]
-        public async Task<IActionResult> UpdateTestimonial(int id)
+        public async Task<IActionResult> UpdateRoom(int id)
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"http://localhost:5266/api/Testimonial/{id}");
+            var responseMessage = await client.GetAsync($"http://localhost:5266/api/Room/{id}");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<TestimonialViewModel>(jsonData);
+                var values = JsonConvert.DeserializeObject<UpdateRoomDto>(jsonData);
                 return View(values);
             }
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> UpdateTestimonial(TestimonialViewModel model)
+        public async Task<IActionResult> UpdateRoom(UpdateRoomDto updateRoomDto)
         {
             var client = _httpClientFactory.CreateClient();
-            var jsonData = JsonConvert.SerializeObject(model);
+            var jsonData = JsonConvert.SerializeObject(updateRoomDto);
             StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            var responseMessage = await client.PutAsync("http://localhost:5266/api/Testimonial/", stringContent);
+            var responseMessage = await client.PutAsync("http://localhost:5266/api/Room/", stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
