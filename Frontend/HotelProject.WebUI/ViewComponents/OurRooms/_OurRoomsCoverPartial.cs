cbc00f4fae6c5ab7cce6005ab380ae68d364883f
furkaniscn <furkan.iscn@outlook.com>
2024-09-05 00:34:56 +0300

    NecessaryUpdatesHasBeenDone

---
+++ Frontend/HotelProject.WebUI/ViewComponents/OurRooms/_OurRoomsCoverPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.ViewComponents.OurRooms
+{
+    public class _OurRoomsCoverPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
