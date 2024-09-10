e19be3fc91f46fa80936e6b505722151fb67dd4f
furkaniscn <furkan.iscn@outlook.com>
2024-09-11 00:55:15 +0300

    SendMailWithSmtpProtocolHasBeenAdded

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,41 @@
+﻿using HotelProject.WebUI.Models.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminMailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(AdminMailViewModel model)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+            //Kimden
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("HotelierAdmin", "furkan.iscn01@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+            //Kime
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+            //Mail İçeriği
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = model.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+            //Mail Başlığı
+            mimeMessage.Subject = model.Subject;
+            //Smtp Protokolü ile Mail Gönderme İşlemi
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com", 587, false);
+            smtpClient.Authenticate("furkan.iscn01@gmail.com", "rfngwvvenfpeykcd");
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+
+            return View();
+        }
+    }
+}
