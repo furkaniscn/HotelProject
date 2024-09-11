f57fe0057a8ad5b1bf4f3cd98f4fef796830b2dc
furkaniscn <furkan.iscn@outlook.com>
2024-09-12 00:57:54 +0300

    ApiUploadFileProcessHasBeenAdded

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminFileController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminFileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            var stream = new MemoryStream();//Akışı oluşturduk.
+            await file.CopyToAsync(stream);//Dosyayı kopyaladık.
+            var bytes = stream.ToArray();//Akıştaki dosyayı byte olarak tutuyoruz.
+
+            ByteArrayContent byteArrayContent = new ByteArrayContent(bytes);
+            byteArrayContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+            MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+            multipartFormDataContent.Add(byteArrayContent, "file", file.FileName);
+            var httpClient = new HttpClient();
+            await httpClient.PostAsync("http://localhost:5266/api/FileProcess/", multipartFormDataContent);
+
+            return View();
+        }
+    }
+}
