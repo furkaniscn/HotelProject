e1b6f787e2f72ae3acb6f8308c8f00ea9a53029b
furkaniscn <furkan.iscn@outlook.com>
2024-09-24 01:00:55 +0300

    InstagramDatasHasBeenTaken

--- Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
@@ -1,11 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.DashboardDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace HotelProject.WebUI.ViewComponents.Dashboard
 {
     public class _DashboardSubscribeCountPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://instagram-profile1.p.rapidapi.com/getprofileinfo/galatasaray"),
+                Headers =
+    {
+        { "x-rapidapi-key", "f9e819033emsh398da8f02ca9315p1315dcjsn0fdcaa771d2a" },
+        { "x-rapidapi-host", "instagram-profile1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                ResultInstagramFollowersDto instagramFollowersDto = JsonConvert.DeserializeObject<ResultInstagramFollowersDto>(body);
+                ViewBag.instagramFollowers = instagramFollowersDto.followers;
+                ViewBag.instagramFollowing = instagramFollowersDto.following;
+            }
+
+            var client2 = new HttpClient();
+            var request2 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://twitter32.p.rapidapi.com/getProfile?username=galatasaray"),
+                Headers =
+    {
+        { "x-rapidapi-key", "f9e819033emsh398da8f02ca9315p1315dcjsn0fdcaa771d2a" },
+        { "x-rapidapi-host", "twitter32.p.rapidapi.com" },
+    },
+            };
+            using (var response2 = await client2.SendAsync(request2))
+            {
+                response2.EnsureSuccessStatusCode();
+                var body2 = await response2.Content.ReadAsStringAsync();
+                ResultTwitterFollowersDto twitterFollowersDto = JsonConvert.DeserializeObject<ResultTwitterFollowersDto>(body2);
+                ViewBag.twitterFollowers = twitterFollowersDto;
+                ViewBag.twitterFollowing = twitterFollowersDto;
+            }
             return View();
         }
     }
